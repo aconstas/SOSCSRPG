c073bbb184702f5d0883ef56d1ae2dd269b9e66e
aconstas <82304192+aconstas@users.noreply.github.com>
2024-07-23 21:49:52 -0700

    create the world

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,19 +1,19 @@
-﻿using Engine.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Engine.Models;
+using Engine.Factories;
 namespace Engine.ViewModels
 {
     public class GameSession
     {
-        public Player CurrentPlayer {  get; set; }
+        public World CurrentWorld { get; set; }
+        public Player CurrentPlayer { get; set; }
         public Location CurrentLocation { get; set; }
-
-        public GameSession() 
-        { 
+        public GameSession()
+        {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Alex";
             CurrentPlayer.CharacterClass = "Fighter";
@@ -21,13 +21,9 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
-
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house.";
-            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
     }
 }
